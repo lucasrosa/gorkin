860f66a1ff84ff5cb7bd376816e1534d89672175
Lucas Rosa <lucas.furlani.rosa@gmail.com>
2019-10-06 21:25:53 -0300

    Added simple authentication with Okta

--- go.mod
+++ go.mod
@@ -5,4 +5,7 @@ go 1.12
 require (
 	github.com/aws/aws-lambda-go v1.11.1
 	github.com/aws/aws-sdk-go v1.20.13
+	github.com/lestrrat-go/jwx v0.9.0 // indirect
+	github.com/okta/okta-jwt-verifier-golang v0.1.0
+	github.com/pkg/errors v0.8.1 // indirect
 )
--- src/adapters/primary/getFolders/main.go
+++ src/adapters/primary/getFolders/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	s3 "github.com/lucasrosa/gorkin/src/adapters/secondary/object"
 	"github.com/lucasrosa/gorkin/src/corelogic/feature"
+	verifier "github.com/okta/okta-jwt-verifier-golang"
 )
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(request events.APIGatewayProxyRequest) (Response, error) {
+	fmt.Println("request Authorization:")
+	fmt.Println(request.Headers["Authorization"])
+
+	if !isAuthenticated(request.Headers["Authorization"]) {
+		return Response{StatusCode: 401}, nil
+	}
 	repo, err := s3.NewS3FolderRepository()
 
 	if err != nil {
@@ -23,4 +33,31 @@ func Handler(request events.APIGatewayProxyRequest) (Response, error) {
 
 func main() {
 	lambda.Start(Handler)
+}
+
+func isAuthenticated(authHeader string) bool {
+	//authHeader := r.Header.Get("Authorization")
+
+	if authHeader == "" {
+		return false
+	}
+	tokenParts := strings.Split(authHeader, "Bearer ")
+	bearerToken := tokenParts[1]
+
+	tv := map[string]string{}
+	tv["aud"] = "api://default"
+	tv["cid"] = "0oa1iwp6inUyX1mys357" //os.Getenv("SPA_CLIENT_ID")
+	jv := verifier.JwtVerifier{
+		Issuer:           "https://dev-774764.okta.com/oauth2/default", //os.Getenv("ISSUER"),
+		ClaimsToValidate: tv,
+	}
+
+	_, err := jv.New().VerifyAccessToken(bearerToken)
+
+	if err != nil {
+		fmt.Println("Error while authenticating: ", err)
+		return false
+	}
+
+	return true
 }
